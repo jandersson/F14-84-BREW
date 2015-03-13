d4a2ae96eb63f9b3cc1284a0bd8574b8fc9eba3e
Jonas Andersson <jonas@andersson.io>
2015-03-13 02:40:36 -0500

    Create recipe class

--- Brew_UI/recipe.cpp
+++ Brew_UI/recipe.cpp
@@ -1,6 +1,14 @@
 #include "recipe.h"
-
+#include <string>
+#include <QDebug>
+#include <iostream>
+#include <QString>
+using namespace std;
 Recipe::Recipe(QObject *parent) :
     QObject(parent)
 {
+}
+
+void Recipe::setName(QString recipeName){
+    this->recipeName = recipeName;
 }
--- Brew_UI/recipe.h
+++ Brew_UI/recipe.h
@@ -1,8 +1,8 @@
 #ifndef RECIPE_H
 #define RECIPE_H
-
+using namespace std;
 #include <QObject>
-
+#include <QString>
 class Recipe : public QObject
 {
     Q_OBJECT
@@ -10,9 +10,11 @@ public:
     explicit Recipe(QObject *parent = 0);
 
 signals:
-
+    void getName();
 public slots:
-
+    void setName(QString recipeName);
+private:
+    QString recipeName;
 };
 
 #endif // RECIPE_H
