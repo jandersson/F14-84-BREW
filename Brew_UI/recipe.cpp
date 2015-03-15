13323c19f5daf326229d13c14a75671e166949f5
Jonas Andersson <jonas@andersson.io>
2015-03-15 16:16:20 -0500

    Create nested struct mashStep within recipe class

--- Brew_UI/recipe.cpp
+++ Brew_UI/recipe.cpp
@@ -12,6 +12,13 @@ Recipe::Recipe(QObject *parent) :
     importPath = "";
 }
 
+Recipe::mashStep::mashStep(QString _name, QString _type, double _temperature, double _time){
+    name = _name;
+    type = _type;
+    temperature = _temperature;
+    time = _time;
+}
+
 void Recipe::setRecipeName(QString recipeName){
     this->recipeName = recipeName;
     recipeNameChanged();
--- Brew_UI/recipe.h
+++ Brew_UI/recipe.h
@@ -2,12 +2,12 @@
 #define RECIPE_H
 #include <QObject>
 #include <QString>
+#include <QList>
 class Recipe : public QObject
 {
     Q_OBJECT
 public:
     explicit Recipe(QObject *parent = 0);
-
 signals:
     void recipeNameChanged();
 public slots:
@@ -19,6 +19,14 @@ private:
     QString recipeName;
     QString importPath;
     void importRecipe();
+    QList mashSteps;
+    struct mashStep{
+        mashStep(QString _name, QString _type, double _temperature, double _time);
+        QString name;
+        QString type;
+        double temperature;
+        double time;
+    };
 };
 
 #endif // RECIPE_H
