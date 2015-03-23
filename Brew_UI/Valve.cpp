2645db9b582c9341cb84061ab1945213f58669ee
bhalim <bhalim93@hotmail.com>
2015-03-23 16:20:29 -0500

    Adding my automation code to the project

---
+++ Brew_UI/Valve.cpp
@@ -0,0 +1,21 @@
+#include "Valve.h"
+
+
+Valve::Valve()
+{
+}
+
+
+Valve::~Valve()
+{
+}
+
+int Valve_Open()
+{
+	return 0;
+}
+
+int Valve_Close()
+{
+	return 0;
+}
---
+++ Brew_UI/Valve.h
@@ -0,0 +1,10 @@
+#pragma once
+class Valve
+{
+public:
+	Valve();
+	~Valve();
+	int Valve_Open();
+	int Valve_Close();
+};
+
