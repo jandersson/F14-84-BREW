a4e5b75c8c4aedb58b757c3e0902d58796b42e7c
Jonas Andersson <jonas@andersson.io>
2015-03-25 18:13:04 -0500

    Create a temp_probe wrapper class

---
+++ Brew_UI/qthermocouple.h
@@ -0,0 +1,18 @@
+#ifndef QTHERMOCOUPLE_H
+#define QTHERMOCOUPLE_H
+
+#include <QObject>
+
+class Qthermocouple : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Qthermocouple(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // QTHERMOCOUPLE_H
