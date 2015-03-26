28662127a0b57b17eeeb9b862a7655c7beb2cd45
Jonas Andersson <jonas@andersson.io>
2015-03-25 21:55:08 -0500

    Comment out non working functions taking time_t arguments

--- Brew_UI/Timer.h
+++ Brew_UI/Timer.h
@@ -6,6 +6,6 @@ public:
 	~Timer();
 	void CountDown(int, int, int);
 	void CountUp();
-	int SetTimer(struct timeval, time_t); //These ones should work on the pi
-	int CheckTimer(struct timeval, time_t); //THey do not on windows
+//	int SetTimer(struct timeval, time_t); //These ones should work on the pi
+//	int CheckTimer(struct timeval, time_t); //THey do not on windows
 };
