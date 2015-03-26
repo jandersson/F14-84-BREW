f60555a59c65923e03aafbd51e12c183bdf78e6d
Jonas Andersson <jonas@andersson.io>
2015-03-26 10:17:07 -0500

    Add minute, second, hour member variables

--- Brew_UI/Timer.h
+++ Brew_UI/Timer.h
@@ -8,4 +8,8 @@ public:
 	void CountUp();
 //	int SetTimer(struct timeval, time_t); //These ones should work on the pi
 //	int CheckTimer(struct timeval, time_t); //THey do not on windows
+private:
+    int hour;
+    int minute;
+    int second;
 };
