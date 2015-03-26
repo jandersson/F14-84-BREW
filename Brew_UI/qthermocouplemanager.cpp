e867ac12d73024fe7892426397fa4ad535837dab
Jonas Andersson <jonas@andersson.io>
2015-03-26 08:27:54 -0500

    implement thermocouplemanager cpp

--- Brew_UI/qthermocouplemanager.cpp
+++ Brew_UI/qthermocouplemanager.cpp
@@ -4,3 +4,12 @@ QthermocoupleManager::QthermocoupleManager(QObject *parent) :
     QObject(parent)
 {
 }
+
+void QthermocoupleManager::addThermocouple(Qthermocouple *thermocouple){
+    m_thermocoupleList.append(thermocouple);
+    emit thermocoupleListChanged();
+}
+
+QQmlListProperty<Qthermocouple> QthermocoupleManager::getThermocoupleList(){
+    return QQmlListProperty<Qthermocouple>(this, m_thermocoupleList);
+}
